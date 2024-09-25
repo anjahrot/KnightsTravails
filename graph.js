d94c84489748a22afc3899e6c2ada58b8b7ac8e5
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-09-25 21:50:58 +0200

    Implemented BFS algorithm and a list to save each traversed nodes parent, to be able to trace the shortest path once it is found. Code works for some inputs, but not all... have to find bug!

--- graph.js
+++ graph.js
@@ -1,5 +1,40 @@
-//Build graph for moving on a board with a certain dimension (e.g. 8*8 for chessboard)
-buildGraph = (dimension) => {
+//import Linkedlist from '../LinkedList/linkedList.js'
+
+const breadthFirstShortestPath = (graph, start, end) => {
+    const visited = new Set([JSON.stringify(start)]);
+    const queue = [[start, 0]];
+    const prev = {[JSON.stringify(start)]: null};
+    
+    while(queue.length > 0){
+        let [currNode, distance] = queue.shift(); //Destructure to get node, distance and parentnode
+        if(JSON.stringify(currNode) === JSON.stringify(end)) {
+            console.log(`You made it in ${distance} moves. Here is your path: `);
+            let path = [];
+            let curr = JSON.stringify(currNode);
+            while (curr) {
+                path.push(curr);  //Adds parent node to path-array
+                curr = prev[curr];   //Finds parent node in prevarray
+            }
+            while (path.length > 0){
+                let node = path.pop();
+                console.log(node);
+            }
+            return;
+        }
+        for(let next of graph[currNode]){
+            if(!visited.has(JSON.stringify(next))){
+                visited.add(JSON.stringify(next));
+                queue.push([next, distance + 1]);
+                prev[JSON.stringify(next)] = JSON.stringify(currNode); 
+            }
+        }
+    }
+    
+}
+
+//Build graph for moving on a board with a dimension passed as argument (e.g. 8*8 for chessboard)
+//potentialNext function implemented for knights movement on the board
+const buildGraph = (dimension) => {
     const graph =  {};
     
     for(let x=0; x<dimension; x++) {
@@ -17,5 +52,6 @@ buildGraph = (dimension) => {
     return graph;
 }
 
-const graph = buildGraph(8);
-console.log(graph);
+const boardList = buildGraph(8);
+
+console.log(breadthFirstShortestPath(boardList, [0,0], [3,3]));
